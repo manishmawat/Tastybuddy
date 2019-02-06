6e2a656ec7c0a2bfb74602d5ba6eb1737440dd6e
Manish Mawatwal <manish.mawat@gmail.com>
2019-02-06 14:50:05 -0500

    Adding model classes and modified components for shopping list and recipe pages

--- src/app/shopping-list/shoppinglist.component.ts
+++ src/app/shopping-list/shoppinglist.component.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
     selector: 'app-shoppinglist',
     templateUrl: 'shoppinglist.component.html'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
 
+    constructor() {}
+    ngOnInit() {
+    }
 }
