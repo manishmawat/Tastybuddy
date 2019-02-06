6f75c3a1bac45f1dc266f9a625b8dc44a51e0dfc
Manish Mawatwal <manish.mawat@gmail.com>
2019-02-05 23:08:11 -0500

    Adding more components

---
+++ src/app/shopping-list/shoppinglist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-shoppinglist',
+    templateUrl: 'shoppinglist.component.html'
+})
+export class ShoppingListComponent {
+
+}
