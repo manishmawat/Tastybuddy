d1e702a79f84ea944f374c8fd8e817856137a2a3
Manish Mawatwal <manish.mawat@gmail.com>
2019-02-06 22:06:00 -0500

    Adding dropdown behavior on the component.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  // @ViewChild('local-variable-name') is used to get the value in component from the html page.
+  // @ViewChild('inputName') nameInputRef: ElementRef;
+  // @ViewChild('inputAmount') amountInputRef: ElementRef;
+  @Output() itemAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit() {
   }
 
+  AddItemToShoppingList(name: string, amount: number) {
+    this.itemAdded.emit(new Ingredient(name, amount));
+  }
 }
